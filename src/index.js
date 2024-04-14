58ffbed405956a72d76c23faa4748312bfc53436
Daham <dahampositha33@gmail.com>
2024-04-14 16:08:42 +0530

    Modify index.js

--- src/index.js
+++ src/index.js
@@ -15,6 +15,8 @@ const config = {
     scope: ["openid", "profile"]
 };
 
+console.log(config)
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
